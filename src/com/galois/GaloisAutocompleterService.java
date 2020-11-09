4bed5ddec499071bdd377445cf07ad6860695126
Gabriel Tamujo Meyrer <gabriel.meyrer@cwi.com.br>
2020-11-09 09:11:38 -0300

    refact: overriding fillCompletionVariants

--- src/com/galois/GaloisAutocompleterService.java
+++ src/com/galois/GaloisAutocompleterService.java
@@ -1,12 +1,19 @@
 package com.galois;
 
+import com.galois.dto.request.PredictionAcceptedRequestDto;
+import com.galois.dto.request.PredictionRequestDto;
+import com.galois.dto.result.PredictionListResultDto;
+import com.galois.dto.result.PredictionResultDto;
+import com.google.gson.Gson;
+import com.intellij.openapi.application.ex.ApplicationUtil;
+import com.intellij.openapi.progress.ProgressManager;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class GaloisAutocompleterService {
 
@@ -15,37 +22,49 @@ public class GaloisAutocompleterService {
 
     private final HttpClient client;
 
+    private final Gson gson;
+
     public GaloisAutocompleterService(@NotNull String baseUrl) {
         this.baseUrl = baseUrl;
+        this.gson = new Gson();
         this.client = HttpClient.newHttpClient();
     }
 
-    public String predict(String requestBody) {
+    public List<PredictionResultDto> predict(@NotNull final PredictionRequestDto predictionRequestDto) {
         final String AUTOCOMPLETE_ROUTE = "autocomplete";
+
         try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(baseUrl + AUTOCOMPLETE_ROUTE))
-                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                    .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-            return response.statusCode() == 200 ? response.body() : null;
-        } catch (InterruptedException | IOException e) {
+            return ApplicationUtil.runWithCheckCanceled(() -> {
+                final HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(baseUrl + AUTOCOMPLETE_ROUTE))
+                        .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(predictionRequestDto)))
+                        .build();
+                final HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200
+                        && response.body() != null
+                        && !response.body().isEmpty()) {
+                    return gson.fromJson(response.body(), PredictionListResultDto.class).getResult();
+                }
+                return null;
+            }, ProgressManager.getInstance().getProgressIndicator());
+
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+
     }
 
-    public void reportAcceptedPrediction(String requestBody) {
-        final String AUTOCOMPLETE_ROUTE = "acceptance";
+    public void reportAcceptedPrediction(@NotNull final PredictionAcceptedRequestDto predictionAcceptedRequestDto) {
+        final String ACCEPTANCE_ROUTE = "acceptance";
         try {
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(baseUrl + AUTOCOMPLETE_ROUTE))
-                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .uri(URI.create(baseUrl + ACCEPTANCE_ROUTE))
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(predictionAcceptedRequestDto)))
                     .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (InterruptedException | IOException e) {
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/com/galois/GaloisCompleterContributor.java
+++ src/com/galois/GaloisCompleterContributor.java
@@ -1,27 +1,113 @@
 package com.galois;
 
+import com.galois.dto.request.PredictionAcceptedRequestDto;
+import com.galois.dto.request.PredictionRequestDto;
+import com.galois.dto.result.PredictionResultDto;
+import com.galois.enums.PredictionType;
 import com.intellij.codeInsight.completion.CompletionContributor;
-import com.intellij.codeInsight.completion.CompletionType;
-import com.intellij.psi.JavaTokenType;
-import com.intellij.psi.PsiElement;
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.codeInsight.lookup.LookupElementPresentation;
+import com.intellij.codeInsight.lookup.LookupElementRenderer;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
 
-import static com.intellij.patterns.PlatformPatterns.psiElement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author GabrielTamujo
  */
 public class GaloisCompleterContributor extends CompletionContributor {
 
-    public GaloisCompleterContributor() {
-        extend(CompletionType.BASIC,
-                psiElement(JavaTokenType.IDENTIFIER),
-                new GaloisCompleterProvider("http://localhost:3030/"));
-    }
+    private final GaloisAutocompleterService galoisAutocompleterService = new GaloisAutocompleterService("http://localhost:3030/");
 
     @Override
-    public boolean invokeAutoPopup(@NotNull PsiElement position, char typeChar) {
-        return true;
+    public void fillCompletionVariants(@NotNull CompletionParameters completionParameters, @NotNull CompletionResultSet completionResultSet) {
+
+        completionResultSet.restartCompletionOnAnyPrefixChange();
+
+        final String contextText = getContextText(completionParameters);
+        final String prefix = completionResultSet.getPrefixMatcher().getPrefix();
+        final String suffix = getCursorSuffix(completionParameters);
+
+        final List<PredictionResultDto> completionResults =
+                galoisAutocompleterService.predict(new PredictionRequestDto(contextText));
+
+        if (completionResults != null) {
+
+            final List<LookupElement> lookupElements = mapLookupElement(completionResults, prefix, suffix);
+
+            if (!lookupElements.isEmpty()) {
+                completionResultSet.addAllElements(lookupElements);
+            }
+        }
+
+    }
+
+    private List<LookupElement> mapLookupElement(@NotNull final List<PredictionResultDto> completions,
+                                                 @NotNull final String prefix,
+                                                 @NotNull final String suffix) {
+
+        List<LookupElement> elementList = new ArrayList<>();
+        completions.forEach(prediction -> {
+            if (!prediction.getPrediction().isBlank()) {
+                elementList.add(createLookupElement(prediction, prefix, suffix));
+            }
+        });
+        return elementList;
+    }
+
+    private LookupElementBuilder createLookupElement(@NotNull final PredictionResultDto prediction,
+                                                     @NotNull final String prefix,
+                                                     @NotNull final String suffix) {
+
+        final String completion = prediction.getPrediction();
+        final GaloisLookUpElement galoisLookUpElement = new GaloisLookUpElement(
+                prefix.isBlank() ? completion : prefix + completion,
+                prediction.getType(),
+                suffix);
+
+        return LookupElementBuilder.create(galoisLookUpElement, galoisLookUpElement.getInsertText())
+                .withInsertHandler((context, item) -> {
+                    final GaloisLookUpElement lookupElement = (GaloisLookUpElement) item.getObject();
+                    final int end = context.getTailOffset();
+                    if (lookupElement.getType().equals(PredictionType.MULTIPLE_TOKENS.name())) {
+                        context.getDocument().deleteString(end, end + lookupElement.getSuffix().length());
+                    }
+                    galoisAutocompleterService.reportAcceptedPrediction(
+                            new PredictionAcceptedRequestDto(lookupElement.getInsertText(), lookupElement.getType()));
+                }).withRenderer(new LookupElementRenderer<LookupElement>() {
+                    @Override
+                    public void renderElement(LookupElement lookupElement, LookupElementPresentation lookupElementPresentation) {
+                        final GaloisLookUpElement prediction = (GaloisLookUpElement) lookupElement.getObject();
+                        lookupElementPresentation.setItemText(prediction.getInsertText());
+                        lookupElementPresentation.setItemTextBold(true);
+                    }
+                });
+    }
+
+    private String getContextText(@NotNull final CompletionParameters completionParameters) {
+        final int MAX_OFFSET = 1000;
+        final String START_OF_TEXT_TOKEN = "<|startoftext|>";
+        final Document document = completionParameters.getEditor().getDocument();
+        final int middle = completionParameters.getOffset();
+        final int begin = Integer.max(0, middle - MAX_OFFSET);
+        final String documentRangeText = document.getText(new TextRange(begin, middle));
+
+        return begin == 0 ? START_OF_TEXT_TOKEN + '\n' + documentRangeText : documentRangeText;
+    }
+
+    private String getCursorSuffix(@NotNull final CompletionParameters parameters) {
+        final Document document = parameters.getEditor().getDocument();
+        final int cursorPosition = parameters.getOffset();
+        final int lineNumber = document.getLineNumber(cursorPosition);
+        final int lineEnd = document.getLineEndOffset(lineNumber);
+
+        return document.getText(TextRange.create(cursorPosition, lineEnd)).trim();
     }
 
 }
