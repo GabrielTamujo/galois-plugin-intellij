7d38e458a1790112a07816b989430e86704d8f7c
Gabriel Tamujo Meyrer <gabriel.meyrer@cwi.com.br>
2020-12-23 12:53:48 -0300

    wip

--- src/com/galois/GaloisCompleterProvider.java
+++ src/com/galois/GaloisCompleterProvider.java
@@ -3,22 +3,40 @@ package com.galois;
 import com.galois.dto.request.PredictionRequestDto;
 import com.galois.dto.result.PredictionResultDto;
 import com.galois.enums.PredictionType;
+import com.intellij.codeInsight.CodeSmellInfo;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.codeInsight.lookup.LookupElementRenderer;
+import com.intellij.codeInsight.problems.WolfTheProblemSolverImpl;
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.codeInspection.java19modules.JavaModuleNamingInspection;
+import com.intellij.codeInspection.javaDoc.JavaDocLocalInspection;
+import com.intellij.codeInspection.reflectiveAccess.JavaReflectionInvocationInspection;
+import com.intellij.codeInspection.ui.InspectionTree;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.JavaTokenType;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiJavaToken;
+import com.intellij.openapi.vcs.CodeSmellDetector;
+import com.intellij.openapi.vcs.changes.PsiChangeTracker;
+import com.intellij.patterns.PsiModifierListOwnerPattern;
+import com.intellij.problems.WolfTheProblemSolver;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -42,10 +60,12 @@ public class GaloisCompleterProvider extends CompletionProvider<CompletionParame
         final String prefix = completionResultSet.getPrefixMatcher().getPrefix();
         final String suffix = getCursorSuffix(completionParameters);
 
-        final List<PredictionResultDto> completionResults =
+        List<PredictionResultDto> completionResults =
                 galoisAutocompleterService.predict(new PredictionRequestDto(contextText));
 
-        if (completionResults != null) {
+        //final List<PredictionResultDto> validSuggestions = deleteInvalidSuggestions(completionParameters, completionResults);
+
+        if (completionResults != null ) {
 
             final List<LookupElement> lookupElements = mapLookupElement(completionResults, prefix, suffix);
 
@@ -60,6 +80,35 @@ public class GaloisCompleterProvider extends CompletionProvider<CompletionParame
             return !((PsiJavaToken) psiElement).getTokenType().equals(JavaTokenType.IDENTIFIER);
         }
         return true;
+    }
+
+    private List<PredictionResultDto> deleteInvalidSuggestions(@NotNull CompletionParameters completionParameters, List<PredictionResultDto> predictions) {
+        PsiFileFactory fileFactory = PsiFileFactory.getInstance(completionParameters.getOriginalFile().getProject());
+        for (PredictionResultDto prediction : predictions) {
+            PsiFile tempFile = null;
+            try {
+                tempFile = fileFactory.createFileFromText(JavaLanguage.INSTANCE, getFullContextText(completionParameters) + prediction);
+                String text = tempFile.getText();
+
+                //Using DaemonCode
+                //DaemonCodeAnalyzer codeAnalyzer = DaemonCodeAnalyzer.getInstance(tempFile.getProject());
+                //List<HighlightInfo> errors = codeAnalyzer.(tempFile, completionParameters.getEditor().getDocument(),ProgressManager.getInstance().getProgressIndicator());
+
+                //Using specific inspectors
+                //InspectionManager inspectionManager = InspectionManager.getInstance(tempFile.getProject());
+                //JavaDocLocalInspection inspection = new JavaDocLocalInspection();
+                //JavaReflectionInvocationInspection inspection = new JavaReflectionInvocationInspection();
+                //JavaModuleNamingInspection inspection = new JavaModuleNamingInspection();
+                //List<ProblemDescriptor> checked = inspection.processFile(tempFile, inspectionManager);
+
+                //Using CodeSmell API
+                List<CodeSmellInfo> codeSmells = CodeSmellDetector.getInstance(tempFile.getProject()).findCodeSmells(Collections.singletonList(tempFile.getVirtualFile()));
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return predictions;
     }
 
     private List<LookupElement> mapLookupElement(@NotNull final List<PredictionResultDto> completions,
@@ -111,6 +160,11 @@ public class GaloisCompleterProvider extends CompletionProvider<CompletionParame
         final String documentRangeText = document.getText(new TextRange(begin, middle));
 
         return begin == 0 ? START_OF_TEXT_TOKEN + '\n' + documentRangeText : documentRangeText;
+    }
+
+    private String getFullContextText(@NotNull final CompletionParameters completionParameters) {
+        final Document document = completionParameters.getEditor().getDocument();
+        return document.getText(new TextRange(0, completionParameters.getOffset()));
     }
 
     private String getCursorSuffix(@NotNull final CompletionParameters parameters) {
